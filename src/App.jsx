3df6fb7e97fd127dafffbf08196c1aca2ff5f34d
VIPUL070 <sutharvipul007@gmail.com>
2025-09-12 17:03:31 +0530

    Installign and implementing React-router in our project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import {Routes ,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
+import CheckOut from './pages/CheckOut.jsx'
+import Orders from './pages/Orders.jsx'
+import Tracking from './pages/Tracking.jsx'
+import './App.css'
+
+ //all the pages in our website : Routes
+ //Route : Add page to our website
+ //path : url path of the page == index shorthand
+ //element : which ele of component to display
+ //Link [to] : go to other page without reloading and faster
 
 function App() {
   return (
-    <>
-     <HomePage />
-    </>
+    <Routes>   
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckOut />} />              
+      <Route path="orders" element={<Orders />} />                            
+      <Route path="tracking" element={<Tracking/>} />                            
+    </Routes>
   )
 }
 
